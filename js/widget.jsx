cb39cbfb8fe57af502efbd092f038be9811cc09d
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-08 12:57:16 +0100

    npm i @niivue/niivue

--- js/widget.jsx
+++ js/widget.jsx
@@ -2,14 +2,33 @@ import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
 import "./widget.css";
 
+import { useRef, useEffect, useState } from "react";
+import { Niivue } from "@niivue/niivue";
+
+const NiiVue = ({ imageUrl }) => {
+  const canvas = useRef();
+  useEffect(() => {
+    const volumeList = [
+      {
+        url: imageUrl,
+      },
+    ];
+    const nv = new Niivue();
+    nv.attachToCanvas(canvas.current);
+    nv.loadVolumes(volumeList);
+  }, [imageUrl]);
+
+  return <canvas ref={canvas} height={480} width={640} />;
+};
+
 export const render = createRender(() => {
-	const [value, setValue] = useModelState("value");
-	return (
-		<button
-			className="anywidget_ipyniivue-counter-button"
-			onClick={() => setValue(value + 1)}
-		>
-			count is {value}
-		</button>
-	);
+  const [value, setValue] = useModelState("value");
+  return (
+    <>
+      <h1>hello</h1>
+      <NiiVue
+        imageUrl={"https://niivue.github.io/niivue-demo-images/mni152.nii.gz"}
+      ></NiiVue>
+    </>
+  );
 });
